168a50971e2beba13e4ffcc33fcc323be6df4166
Jechto <jetof20@student.sdu.dk>
2025-02-10 14:50:59 +0100

    added basic state code

--- assignment-1-internal-dsl-main/mdsd.assignment1/src/main/StateMachine.java
+++ assignment-1-internal-dsl-main/mdsd.assignment1/src/main/StateMachine.java
@@ -1,9 +1,18 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import main.metamodel.Machine;
+import main.metamodel.State;
 
 public class StateMachine {
 
+	private HashMap<String, Integer> variables = new HashMap<String, Integer>();
+	private ArrayList<State> states = new ArrayList<State>();
+	
+	
 	public Machine build() {
 		// TODO Auto-generated method stub
 		return null;
@@ -11,7 +20,9 @@ public class StateMachine {
 
 	public StateMachine state(String string) {
 		// TODO Auto-generated method stub
-		return null;
+		State new_state = new State(string);
+		states.add(new_state);
+		return this;
 	}
 
 	public StateMachine initial() {
@@ -30,23 +41,29 @@ public class StateMachine {
 	}
 
 	public StateMachine integer(String string) {
-		// TODO Auto-generated method stub
-		return null;
+		variables.remove(string);
+		variables.put(string, 0);
+		return this;
 	}
 
 	public StateMachine set(String string, int i) {
-		// TODO Auto-generated method stub
-		return null;
+		variables.remove(string);
+		variables.put(string, i);
+		return this;
 	}
 
 	public StateMachine increment(String string) {
-		// TODO Auto-generated method stub
-		return null;
+		int new_var = variables.get(string);
+		variables.remove(string);
+		variables.put(string, new_var + 1);
+		return this;
 	}
 
 	public StateMachine decrement(String string) {
-		// TODO Auto-generated method stub
-		return null;
+		int new_var = variables.get(string);
+		variables.remove(string);
+		variables.put(string, new_var - 1);
+		return this;
 	}
 
 	public StateMachine ifEquals(String string, int i) {
--- assignment-1-internal-dsl-main/mdsd.assignment1/src/main/metamodel/State.java
+++ assignment-1-internal-dsl-main/mdsd.assignment1/src/main/metamodel/State.java
@@ -3,15 +3,26 @@ package main.metamodel;
 import java.util.List;
 
 public class State {
+	
+	private String name;
+	private List<Transition> transitions;
+	
+	
+	public State(String name) {
+		this.name = name;
+	}
 
 	public Object getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
+	}
+	
+	public void addTransition(Transition transition) {
+		transitions.add(transition);
 	}
 
 	public List<Transition> getTransitions() {
 		// TODO Auto-generated method stub
-		return null;
+		return transitions;
 	}
 
 	public Transition getTransitionByEvent(String string) {
